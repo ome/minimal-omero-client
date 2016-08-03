8091d062bb67ce53a7ba90fc1c6de8d346b10ba4
Sebastien Besson <seb.besson@gmail.com>
2016-08-03 09:13:06 +0100

    Revert "Add a 4 seconds pause to prevent NPE"
    This reverts commit 8d20169e5bbb6c62b0b161d9934ce264514e958f.

--- src/main/java/com/example/SimpleConnection.java
+++ src/main/java/com/example/SimpleConnection.java
@@ -96,7 +96,6 @@ public class SimpleConnection {
             //Do something e.g. loading user's data.
             //Load the projects/datasets owned by the user currently logged in.
             client.loadProjects();
-            Thread.sleep(4000);
         } catch (Exception e) {
             System.out.println(e);
         } finally {
